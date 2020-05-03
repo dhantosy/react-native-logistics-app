62fdd9c9c3d676d5798d56b78e730bcdeda0e1aa
dhantosy <dhantosy@gmail.com>
2020-05-03 12:44:24 +0700

    updated mockup data

--- LogisticsApp/app/data/dataPickUpDetails.js
+++ LogisticsApp/app/data/dataPickUpDetails.js
@@ -4,7 +4,7 @@ export const dataPickUpDetails = [
     name_merchant: 'PT Sinar Utama',
     name_owner: 'Ronny Utomo',
     pickup_time: '09:00 - 12:00',
-    phone: 08771234569,
+    phone: '08771234569',
     address: 'Grand Depok City Blok A8 No. 61A, Jl. Grand Boul Depok City - Cluster Mawar',
   }
 ]
--- LogisticsApp/app/data/dataPickUpRequest.js
+++ LogisticsApp/app/data/dataPickUpRequest.js
@@ -1,44 +1,58 @@
 export const dataPickUpRequest = [
   {
     id: 1,
-    quantity: 3,
-    weight: 5,
+    quantity: '3',
+    weight: '5',
     pickup_time: '09:00 - 12:00',
-    address: 'Grand Depok City Blok A8 No. 61A, Jl. Grand Boul Depok City'
+    address: 'Grand Depok City Blok A8 No. 61A, Jl. Grand Boul Depok City',
+    image_url: 'https://source.unsplash.com/nXhND4y3riM/100x100/'
   },
   {
     id: 2,
-    quantity: 1,
-    weight: 2,
+    quantity: '1',
+    weight: '2',
     pickup_time: '12:00 - 15:00',
-    address: 'Jl Letjen S Parman Ruko Gateway Bl D/29'
+    address: 'Jl Letjen S Parman Ruko Gateway Bl D/29',
+    image_url: 'https://source.unsplash.com/yiU8G1K85AM/100x100/'
   },
   {
     id: 3,
-    quantity: 3,
-    weight: 4,
+    quantity: '3',
+    weight: '4',
     pickup_time: '12:00 - 15:00',
-    address: 'Prambanan 3 Blok H1 N0. 23, Bekasi, Jawa Barat'
+    address: 'Prambanan 3 Blok H1 N0. 23, Bekasi, Jawa Barat',
+    image_url: 'https://source.unsplash.com/q8kR_ie6WnI/100x100/'
   },
   {
     id: 4,
-    quantity: 5,
-    weight: 4,
+    quantity: '5',
+    weight: '4',
     pickup_time: '15:00 - 18:00',
-    address: 'Jl Raya Sultan Agung Km 29 29,Medan Satria'
+    address: 'Jl Raya Sultan Agung Km 29 29,Medan Satria',
+    image_url: 'https://source.unsplash.com/sxNt9g77PE0/100x100/'
   },
   {
     id: 5,
-    quantity: 2,
-    weight: 1,
+    quantity: '2',
+    weight: '1',
     pickup_time: '15:00 - 18:00',
-    address: 'Jl Kyai Caringin 29-31 Wisma Abadi Bl C-5 Lt 3,Cideng'
+    address: 'Jl Kyai Caringin 29-31 Wisma Abadi Bl C-5 Lt 3,Cideng',
+    image_url: 'https://source.unsplash.com/dd1M0Ji9xHQ/100x100/'
   },
   {
     id: 6,
-    quantity: 6,
-    weight: 10,
+    quantity: '6',
+    weight: '10',
     pickup_time: '15:00 - 18:00',
-    address: 'Jl. Citarum No. 35, Jawa Barat'
+    address: 'Jl. Citarum No. 35, Jawa Barat',
+    image_url: 'https://source.unsplash.com/6dfYAFmdoY4/100x100/'
+  },
+  {
+    id: 7,
+    quantity: '18',
+    weight: '2',
+    pickup_time: '18:00 - 21:00',
+    address: 'Jl. Bintaro Jaya XVII No. 61, Tangerang Selatan',
+    image_url: 'https://source.unsplash.com/ViR3a0rx18w/100x100/'
   }
 ]
