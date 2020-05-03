c7ad960161ffc58f933cf4b44c916154c11b27b3
dhantosy <dhantosy@gmail.com>
2020-05-03 16:38:31 +0700

    updated mockup data

--- LogisticsApp/app/data/dataPickUpDetails.js
+++ LogisticsApp/app/data/dataPickUpDetails.js
@@ -1,10 +1,9 @@
-export const dataPickUpDetails = [
-  {
-    id: 1,
-    name_merchant: 'PT Sinar Utama',
-    name_owner: 'Ronny Utomo',
-    pickup_time: '09:00 - 12:00',
-    phone: '08771234569',
-    address: 'Grand Depok City Blok A8 No. 61A, Jl. Grand Boul Depok City - Cluster Mawar',
-  }
-]
+export const dataPickUpDetails = {
+  id: 1,
+  name_merchant: 'PT Sinar Utama',
+  name_owner: 'Ronny Utomo',
+  pickup_time: '09:00 - 12:00',
+  phone: '08771234569',
+  address: 'Grand Depok City Blok A8 No. 61A, Jl. Grand Boul Depok City - Cluster Mawar',
+  image_url: 'https://source.unsplash.com/nXhND4y3riM/100x100/'
+}
