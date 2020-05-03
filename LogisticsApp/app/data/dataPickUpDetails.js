ba26c4c9729ab2d55bac51a4802459fae9e2b3a7
dhantosy <dhantosy@gmail.com>
2020-05-03 09:01:39 +0700

    added mockup data

---
+++ LogisticsApp/app/data/dataPickUpDetails.js
@@ -0,0 +1,10 @@
+export const dataPickUpDetails = [
+  {
+    id: 1,
+    name_merchant: 'PT Sinar Utama',
+    name_owner: 'Ronny Utomo',
+    pickup_time: '09:00 - 12:00',
+    phone: 08771234569,
+    address: 'Grand Depok City Blok A8 No. 61A, Jl. Grand Boul Depok City - Cluster Mawar',
+  }
+]
---
+++ LogisticsApp/app/data/dataPickUpRequest.js
@@ -0,0 +1,44 @@
+export const dataPickUpRequest = [
+  {
+    id: 1,
+    quantity: 3,
+    weight: 5,
+    pickup_time: '09:00 - 12:00',
+    address: 'Grand Depok City Blok A8 No. 61A, Jl. Grand Boul Depok City'
+  },
+  {
+    id: 2,
+    quantity: 1,
+    weight: 2,
+    pickup_time: '12:00 - 15:00',
+    address: 'Jl Letjen S Parman Ruko Gateway Bl D/29'
+  },
+  {
+    id: 3,
+    quantity: 3,
+    weight: 4,
+    pickup_time: '12:00 - 15:00',
+    address: 'Prambanan 3 Blok H1 N0. 23, Bekasi, Jawa Barat'
+  },
+  {
+    id: 4,
+    quantity: 5,
+    weight: 4,
+    pickup_time: '15:00 - 18:00',
+    address: 'Jl Raya Sultan Agung Km 29 29,Medan Satria'
+  },
+  {
+    id: 5,
+    quantity: 2,
+    weight: 1,
+    pickup_time: '15:00 - 18:00',
+    address: 'Jl Kyai Caringin 29-31 Wisma Abadi Bl C-5 Lt 3,Cideng'
+  },
+  {
+    id: 6,
+    quantity: 6,
+    weight: 10,
+    pickup_time: '15:00 - 18:00',
+    address: 'Jl. Citarum No. 35, Jawa Barat'
+  }
+]
