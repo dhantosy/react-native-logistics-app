fda9cce010004a753a6de65e9dac4aa3b4302af2
dhantosy <dhantosy@gmail.com>
2020-05-03 12:45:53 +0700

    added global colors variable

---
+++ LogisticsApp/app/config/colors.js
@@ -0,0 +1,9 @@
+export default {
+  whiteBackground: '#fff',
+  blackText: '#000',
+  redText: '#FC4336',
+  subtleText: '#9a9a9a',
+  border: '#ccc',
+  link: '#007AFF',
+  rowUnderLay: 'rgba(154, 154, 154, 0.25)',
+}
